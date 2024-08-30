d30d02b26ff10972178b77189c565abc6b2a9e34
Parth Jain <42parth.jain@gmail.com>
2024-08-31 00:33:48 +0530

    sih

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain_community.llms import Ollama
 from langchain.prompts import PromptTemplate
@@ -16,10 +16,8 @@ def load_process_and_create_vector_store():
     documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=500)
     texts = text_splitter.split_documents(documents)
-    embeddings = HuggingFaceEmbeddings(
-        model_kwargs={"tokenizer_kwargs": {"clean_up_tokenization_spaces": True}}
-    )
-    vector_store = FAISS.from_texts([text.page_content for text in texts], embeddings)
+    embeddings = OllamaEmbeddings(model="llama3:8b")
+    vector_store = Chroma.from_documents(texts, embeddings)
     return vector_store
 
 
